55e0dc42a1e68713936a55f8ceb1ca8bfe4d540a
Renaldo Mateus <48330827+renaldodev@users.noreply.github.com>
2024-02-18 07:04:56 +0100

    add ficha (#12)

---
+++ src/pages/poco/ficha-geologica.tsx
@@ -0,0 +1,261 @@
+import { Card, Button, Table } from "react-bootstrap";
+import { Column, CustomTable } from "../../components/common/table";
+import { MessageModal } from "../../components/common/message-modal";
+import { service } from "../../service";
+import { ReactNode, useState, Fragment } from "react";
+import { Link } from "react-router-dom";
+import { FormFichaGeologica } from "../../section/register-ficha-geologica";
+
+export function FichaGeologicaListagem({
+  pocoId,
+  action,
+}: {
+  pocoId: number;
+  action?: () => void;
+}) {
+  const { data } = service.fichaGeologica.list.useQuery([
+    "ficha",
+    "geologica",
+    "list",
+  ]);
+  const [fichaId, setFichaId] = useState(0);
+  const [visibility, setVisibility] = useState(1);
+
+  const LeftAction = (context: any) => (
+    <div className="hstack gap-2 fs-15">
+      <Link
+        to="#"
+        className="btn btn-icon btn-sm btn-light"
+        onClick={() => {
+          setFichaId(context.id);
+          setVisibility(2);
+        }}
+      >
+        <i className="ri-focus-line"></i>
+      </Link>
+    </div>
+  );
+
+  const fichas = data?.body?.filter((ficha) => ficha.pocoId === pocoId);
+  const fichasComId = fichas?.map((ficha, index) => ({ ...ficha, id: index }));
+  const ficha = fichasComId?.find((ficha) => ficha.id === fichaId);
+
+  const columns: Column[] = [
+    {
+      dataKey: "descricaoLitologica",
+      title: "Descrição Litológica",
+      display: true,
+    },
+    { dataKey: "idadeDaFormacao", title: "Idade da Formação", display: true },
+    {
+      dataKey: "ambienteDeDeposicao",
+      title: "Ambiente de Deposição",
+      display: true,
+    },
+    { dataKey: "contratos", title: "Contratos", display: true },
+    { dataKey: "rig", title: "Rig", display: true },
+  ];
+  return (
+    <>
+      {visibility === 1 ? (
+        <Card className={"m-5 p-3"}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginBottom: "16px",
+            }}
+          >
+            <h3>Ficha geológica</h3>
+            <Button
+              onClick={() => setVisibility(3)}
+              variant="success"
+              style={{ width: 200 }}
+            >
+              Adicionar ficha
+            </Button>
+          </div>
+          <CustomTable
+            data={fichasComId ?? []}
+            columns={columns}
+            leftAction={LeftAction}
+          />
+        </Card>
+      ) : null}
+      {visibility === 2 ? (
+        <FichaGeologicaDetalhes data={ficha} action={() => setVisibility(1)} />
+      ) : null}
+      {visibility === 3 ? (
+        <FichaGeologicaRegister
+          pocoId={pocoId}
+          action={() => setVisibility(1)}
+        />
+      ) : null}
+    </>
+  );
+}
+
+export const FichaGeologicaDetalhes = ({
+  data,
+  action,
+}: {
+  data: any;
+  action: () => void;
+}) => {
+  const columns: Column[] = [
+    { dataKey: "usuarioRegistroId", title: "ID de Usuário", display: true },
+    { dataKey: "dataRegistro", title: "Data de Registro", display: true },
+    { dataKey: "atualizadoPor", title: "Atualizado Por", display: true },
+    { dataKey: "dataAtualizacao", title: "Data de Atualização", display: true },
+    {
+      dataKey: "fichaGeologicaId",
+      title: "ID da Ficha Geológica",
+      display: true,
+    },
+    {
+      dataKey: "formacaoGeologicaAlvo",
+      title: "Formação Geológica Alvo",
+      display: true,
+    },
+    {
+      dataKey: "descricaoLitologica",
+      title: "Descrição Litológica",
+      display: true,
+    },
+    { dataKey: "idadeDaFormacao", title: "Idade da Formação", display: true },
+    {
+      dataKey: "ambienteDeDeposicao",
+      title: "Ambiente de Deposição",
+      display: true,
+    },
+    { dataKey: "contratos", title: "Contratos", display: true },
+    { dataKey: "rig", title: "Rig", display: true },
+    { dataKey: "mudlogging", title: "Mudlogging", display: true },
+    { dataKey: "mud_Enginnerring", title: "Engenharia de Lama", display: true },
+    {
+      dataKey: "directionalDrilling",
+      title: "Perfuração Direcional",
+      display: true,
+    },
+    { dataKey: "lwd", title: "LWD", display: true },
+    { dataKey: "mwd", title: "MWD", display: true },
+    { dataKey: "wireLine", title: "Wire Line", display: true },
+    { dataKey: "coring", title: "Amostragem", display: true },
+    { dataKey: "core", title: "Núcleo", display: true },
+    { dataKey: "coreLogging", title: "Registro de Núcleo", display: true },
+    { dataKey: "wellSeismic", title: "Sísmica de Poço", display: true },
+    { dataKey: "cement", title: "Cimento", display: true },
+    { dataKey: "testing", title: "Teste", display: true },
+    { dataKey: "wellHead", title: "Cabeça de Poço", display: true },
+    {
+      dataKey: "upperCompletion",
+      title: "Completamento Superior",
+      display: true,
+    },
+    {
+      dataKey: "lowerCompletion",
+      title: "Completamento Inferior",
+      display: true,
+    },
+    { dataKey: "phase", title: "Fase", display: true },
+    {
+      dataKey: "profundidadeDeInicio",
+      title: "Profundidade de Início",
+      display: true,
+    },
+    {
+      dataKey: "profundidadeDeConclusao",
+      title: "Profundidade de Conclusão",
+      display: true,
+    },
+    { dataKey: "mudType", title: "Tipo de Lama", display: true },
+    { dataKey: "dens", title: "Densidade", display: true },
+    { dataKey: "visc", title: "Viscosidade", display: true },
+    { dataKey: "pocoId", title: "ID do Poço", display: true },
+  ];
+  return (
+    <Card className={"m-5 p-3"}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "16px",
+        }}
+      >
+        <h3>Detalhes da ficha geológica</h3>
+        <Button onClick={action} variant="success" style={{ width: 200 }}>
+          voltar
+        </Button>
+      </div>
+
+      <TabelaColum data={data} columns={columns} />
+    </Card>
+  );
+};
+
+export function FichaGeologicaRegister({
+  pocoId,
+  action,
+}: {
+  pocoId: number;
+  action: () => void;
+}) {
+  const [modalActive, setModalActive] = useState({
+    show: false,
+    title: "",
+    message: "",
+  });
+
+  const { mutate } = service.fichaGeologica.create.useMutation({
+    onSuccess() {
+      setModalActive({
+        show: true,
+        message: "Ficha Geologica adicionada com sucesso!",
+        title: "Sucesso",
+      });
+    },
+    onError() {
+      setModalActive({
+        show: true,
+        message: "Algo deu errado!",
+        title: "Erro",
+      });
+    },
+  });
+  return (
+    <Fragment>
+      <MessageModal
+        close={() => {
+          setModalActive((value) => ({ ...value, show: false }));
+        }}
+        {...modalActive}
+      />
+      <FormFichaGeologica
+        pocoId={pocoId}
+        back={action}
+        submit={(body) => mutate({ body })}
+        isLoading={false}
+      />
+    </Fragment>
+  );
+}
+
+interface TableColumProps {
+  data: Record<string, string | number>;
+  columns: { title: ReactNode; dataKey: string }[];
+}
+
+const TabelaColum: React.FC<TableColumProps> = ({ data, columns }) => {
+  return (
+    <Table>
+      <tbody>
+        {columns.map((column, index) => (
+          <tr key={index}>
+            <td>{column.title}</td>
+            <td>{data[column.dataKey]}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
--- src/section/register-ficha-geologica/form.tsx
+++ src/section/register-ficha-geologica/form.tsx
@@ -1,307 +1,306 @@
 import { z } from "zod";
 import { useForm, SubmitHandler } from "react-hook-form";
 import {
-	Button,
-	Col,
-	Row,
-	Spinner,
-	Form as RNForm,
-	Card,
-	InputGroup,
+  Button,
+  Col,
+  Row,
+  Spinner,
+  Form as RNForm,
+  Card,
+  InputGroup,
 } from "react-bootstrap";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Icon } from "@iconify/react";
 import Pageheader from "../../components/pageheader/pageheader";
 import { TipoPocoSelect } from "../register-poco/tipoPoco.component";
 import InputGroupWithLabel from "../../components/common/inputGroup";
+import { TextField } from "../../components/hook-form/textfield";
 
 const schema = z.object({
-	formacaoGeologicaAlvo: z.string(),
-	descricaoLitologica: z.string(),
-	idadeDaFormacao: z.coerce.number(),
-	ambienteDeDeposicao: z.string(),
-	contratos: z.string(),
-	rig: z.string(),
-	mudlogging: z.string(),
-	mud_Enginnerring: z.string(),
-	directionalDrilling: z.string(),
-	lwd: z.string(),
-	mwd: z.string(),
-	wireLine: z.string(),
-	coring: z.string(),
-	core: z.string(),
-	coreLogging: z.string(),
-	wellSeismic: z.string(),
-	cement: z.string(),
-	testing: z.string(),
-	wellHead: z.string(),
-	upperCompletion: z.string(),
-	lowerCompletion: z.string(),
-	phase: z.string(),
-	profundidadeDeInicio: z.coerce.number(),
-	profundidadeDeConclusao: z.coerce.number(),
-	mudType: z.string(),
-	dens: z.coerce.number(),
-	visc: z.coerce.number(),
-	pocoId: z.coerce.number(),
+  formacaoGeologicaAlvo: z.string(),
+  descricaoLitologica: z.string(),
+  idadeDaFormacao: z.coerce.number(),
+  ambienteDeDeposicao: z.string(),
+  contratos: z.string(),
+  rig: z.string(),
+  mudlogging: z.string(),
+  mud_Enginnerring: z.string(),
+  directionalDrilling: z.string(),
+  lwd: z.string(),
+  mwd: z.string(),
+  wireLine: z.string(),
+  coring: z.string(),
+  core: z.string(),
+  coreLogging: z.string(),
+  wellSeismic: z.string(),
+  cement: z.string(),
+  testing: z.string(),
+  wellHead: z.string(),
+  upperCompletion: z.string(),
+  lowerCompletion: z.string(),
+  phase: z.string(),
+  profundidadeDeInicio: z.coerce.number(),
+  profundidadeDeConclusao: z.coerce.number(),
+  mudType: z.string(),
+  dens: z.coerce.number(),
+  visc: z.coerce.number(),
+  pocoId: z.coerce.number(),
 });
 
 type FormValues = z.infer<typeof schema>;
 
 type Props = {
-	submit: (data: FormValues) => void;
-	edit?: boolean;
-	isLoading: boolean;
+  submit: (data: FormValues) => void;
+  edit?: boolean;
+  isLoading: boolean;
+  pocoId: number;
+  back: () => void;
 };
 
-export function FormFichaGeologica({ submit, edit, isLoading }: Props) {
-	const message = edit ? "Atualizar" : "Salvar";
-	const {
-		handleSubmit,
-		control,
-		formState: { errors },
-	} = useForm<FormValues>({
-		resolver: zodResolver(schema),
-	});
-	const onSubmit: SubmitHandler<FormValues> = (data) => submit(data);
-	return (
-		<Card className="m-5 p-3">
-			<Pageheader
-				title="Cadastro"
-				heading=" Ficha Geologica"
-				active="cadastro"
-			/>
-			<form onSubmit={handleSubmit(onSubmit)} className="mt-5">
-				<Row>
-					<InputGroupWithLabel
-						name="formacaoGeologicaAlvo"
-						label="Formacao Geologica Alvo"
-						control={control}
-						error={errors?.formacaoGeologicaAlvo?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="descricaoLitologica"
-						label="Descricão Litólogica"
-						control={control}
-						error={errors?.descricaoLitologica?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="idadeDaFormacao"
-						label="Idade da Formação"
-						control={control}
-						error={errors?.idadeDaFormacao?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="ambienteDeDeposicao"
-						label="Ambiente De Deposicao"
-						control={control}
-						error={errors?.ambienteDeDeposicao?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="rig"
-						label="Rig"
-						control={control}
-						error={errors?.rig?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="core"
-						label="core"
-						control={control}
-						error={errors?.core?.message || ""}
-						icon="material-symbols:description"
-					/>
+export function FormFichaGeologica({ submit, edit, isLoading,pocoId,back }: Props) {
+  const message = edit ? "Atualizar" : "Salvar";
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit: SubmitHandler<FormValues> = (data) => submit(data);
+  return (
+    <Card className="m-5 p-3">
+      <Pageheader
+        title="Cadastro"
+        heading=" Ficha Geologica"
+        active="cadastro"
+      />
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-5">
+        <Row>
+          <InputGroupWithLabel
+            name="formacaoGeologicaAlvo"
+            label="Formacao Geologica Alvo"
+            control={control}
+            error={errors?.formacaoGeologicaAlvo?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="descricaoLitologica"
+            label="Descricão Litólogica"
+            control={control}
+            error={errors?.descricaoLitologica?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="idadeDaFormacao"
+            label="Idade da Formação"
+            control={control}
+            error={errors?.idadeDaFormacao?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="ambienteDeDeposicao"
+            label="Ambiente De Deposicao"
+            control={control}
+            error={errors?.ambienteDeDeposicao?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="rig"
+            label="Rig"
+            control={control}
+            error={errors?.rig?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="core"
+            label="core"
+            control={control}
+            error={errors?.core?.message || ""}
+            icon="material-symbols:description"
+          />
 
-					<InputGroupWithLabel
-						name="contratos"
-						label="Contratos"
-						control={control}
-						error={errors?.contratos?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<Col md={6} className="mb-5">
-						<InputGroup className="mb-3">
-							<InputGroup.Text className="" id="basic-addon1">
-								<Icon icon="ph:wall-fill" />
-							</InputGroup.Text>
-							<TipoPocoSelect
-								control={control}
-								error={errors.pocoId?.message || ""}
-								label="Poço"
-								name="pocoId"
-							/>
-						</InputGroup>
-					</Col>
-					<InputGroupWithLabel
-						name="mudlogging"
-						label="Mudlogging"
-						control={control}
-						error={errors?.mudlogging?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="mud_Enginnerring"
-						label="Mud Engineering"
-						control={control}
-						error={errors?.mud_Enginnerring?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="directionalDrilling"
-						label="Directional Drilling"
-						control={control}
-						error={errors?.directionalDrilling?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="lwd"
-						label="LWD"
-						control={control}
-						error={errors?.lwd?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="mwd"
-						label="MWD"
-						control={control}
-						error={errors?.mwd?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="wireLine"
-						label="Wire Line"
-						control={control}
-						error={errors?.wireLine?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="profundidadeDeInicio"
-						label="Profundidade de Início"
-						control={control}
-						error={errors?.profundidadeDeInicio?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="profundidadeDeConclusao"
-						label="Profundidade de Conclusão"
-						control={control}
-						error={errors?.profundidadeDeConclusao?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="mudType"
-						label="Mud Type"
-						control={control}
-						error={errors?.mudType?.message || ""}
-						icon="material-symbols:description"
-					/>
+          <InputGroupWithLabel
+            name="contratos"
+            label="Contratos"
+            control={control}
+            error={errors?.contratos?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="mudlogging"
+            label="Mudlogging"
+            control={control}
+            error={errors?.mudlogging?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="mud_Enginnerring"
+            label="Mud Engineering"
+            control={control}
+            error={errors?.mud_Enginnerring?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="directionalDrilling"
+            label="Directional Drilling"
+            control={control}
+            error={errors?.directionalDrilling?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="lwd"
+            label="LWD"
+            control={control}
+            error={errors?.lwd?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="mwd"
+            label="MWD"
+            control={control}
+            error={errors?.mwd?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="wireLine"
+            label="Wire Line"
+            control={control}
+            error={errors?.wireLine?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="profundidadeDeInicio"
+            label="Profundidade de Início"
+            control={control}
+            error={errors?.profundidadeDeInicio?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="profundidadeDeConclusao"
+            label="Profundidade de Conclusão"
+            control={control}
+            error={errors?.profundidadeDeConclusao?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="mudType"
+            label="Mud Type"
+            control={control}
+            error={errors?.mudType?.message || ""}
+            icon="material-symbols:description"
+          />
 
-					<InputGroupWithLabel
-						name="visc"
-						label="Viscosity"
-						control={control}
-						error={errors?.visc?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="coreLogging"
-						label="coreLogging"
-						control={control}
-						error={errors?.coreLogging?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="wellSeismic"
-						label="wellSeismic"
-						control={control}
-						error={errors?.wellSeismic?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="cement"
-						label="cement"
-						control={control}
-						error={errors?.cement?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="testing"
-						label="testing"
-						control={control}
-						error={errors?.testing?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="wellHead"
-						label="wellHead"
-						control={control}
-						error={errors?.wellHead?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="upperCompletion"
-						label="upperCompletion"
-						control={control}
-						error={errors?.upperCompletion?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="lowerCompletion"
-						label="lowerCompletion"
-						control={control}
-						error={errors?.lowerCompletion?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="phase"
-						label="phase"
-						control={control}
-						error={errors?.phase?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="coring"
-						label="coring"
-						control={control}
-						error={errors?.coring?.message || ""}
-						icon="material-symbols:description"
-					/>
-					<InputGroupWithLabel
-						name="dens"
-						label="Density"
-						control={control}
-						error={errors?.dens?.message || ""}
-						icon="material-symbols:description"
-					/>
+          <InputGroupWithLabel
+            name="visc"
+            label="Viscosity"
+            control={control}
+            error={errors?.visc?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="coreLogging"
+            label="coreLogging"
+            control={control}
+            error={errors?.coreLogging?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="wellSeismic"
+            label="wellSeismic"
+            control={control}
+            error={errors?.wellSeismic?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="cement"
+            label="cement"
+            control={control}
+            error={errors?.cement?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="testing"
+            label="testing"
+            control={control}
+            error={errors?.testing?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="wellHead"
+            label="wellHead"
+            control={control}
+            error={errors?.wellHead?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="upperCompletion"
+            label="upperCompletion"
+            control={control}
+            error={errors?.upperCompletion?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="lowerCompletion"
+            label="lowerCompletion"
+            control={control}
+            error={errors?.lowerCompletion?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="phase"
+            label="phase"
+            control={control}
+            error={errors?.phase?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="coring"
+            label="coring"
+            control={control}
+            error={errors?.coring?.message || ""}
+            icon="material-symbols:description"
+          />
+          <InputGroupWithLabel
+            name="dens"
+            label="Density"
+            control={control}
+            error={errors?.dens?.message || ""}
+            icon="material-symbols:description"
+          />
 
-					<Col md={12}>
-						<Button
-							type="submit"
-							variant="primary"
-							className="me-3"
-							style={{ padding: "5px 60px" }}
-						>
-							{isLoading ? (
-								<Spinner animation="border" variant="light" size="sm" />
-							) : (
-								message
-							)}
-						</Button>
-						<Button
-							type="submit"
-							variant="secondary"
-							className=""
-							style={{ padding: "5px 60px" }}
-						>
-							Cancelar
-						</Button>
-					</Col>
-				</Row>
-			</form>
-		</Card>
-	);
+          <TextField
+            control={control}
+            style={{ visibility: "hidden" }}
+            error={errors.pocoId?.message || ""}
+            label=""
+            name="pocoId"
+            defaultValue={pocoId}
+          />
+
+          <Col md={12}>
+            <Button
+              type="submit"
+              variant="success"
+              className="me-3"
+              style={{ padding: "5px 60px" }}
+            >
+              {isLoading ? (
+                <Spinner animation="border" variant="light" size="sm" />
+              ) : (
+                message
+              )}
+            </Button>
+            <Button
+              variant="dark"
+              className=""
+              style={{ padding: "5px 60px" }}
+              onClick={back}
+            >
+              Cancelar
+            </Button>
+          </Col>
+        </Row>
+      </form>
+    </Card>
+  );
 }
