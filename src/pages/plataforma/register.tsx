5344a8ddb9aca7d8f5e852f3ee3f1b8afdd94ec5
Renaldo Mateus <48330827+renaldodev@users.noreply.github.com>
2024-02-18 04:42:09 +0100

    add poco (#11)

--- src/pages/plataforma/register.tsx
+++ src/pages/plataforma/register.tsx
@@ -1,22 +1,46 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Form } from "../../section/register-plataforma";
 import { service } from "../../service";
 import { enqueueSnackbar } from "notistack";
+import { MessageModal } from "../../components/common/message-modal";
 
 export function PlataformaRegister() {
-	const { mutate } = service.plataforma.create.useMutation({
-		onSuccess() {
-			enqueueSnackbar("Plataforma adicionada com sucesso");
-		},
-		onError(error) {
-			enqueueSnackbar(`Algo deu errado ${JSON.stringify(error)}`, {
-				variant: "error",
-			});
-		},
-	});
-	return (
-		<Fragment>
-			<Form submit={(body) => mutate({ body })} isLoading={false} />
-		</Fragment>
-	);
+  const [modalActive, setModalActive] = useState({
+    show: false,
+    title: "",
+    message: "",
+  });
+
+  const [limpar, setLimpar] = useState(0);
+
+  const { mutate } = service.plataforma.create.useMutation({
+    onSuccess() {
+      // enqueueSnackbar("Plataforma adicionado com sucesso");
+      setModalActive({
+        show: true,
+        message: "Plataforma adicionado com sucesso!",
+        title: "Sucesso",
+      });
+    },
+    onError() {
+      // enqueueSnackbar("Algo deu errado", { variant: "error" });
+      setModalActive({
+        show: true,
+        message: "Algo deu errado!",
+        title: "Erro",
+      });
+    },
+  });
+  return (
+    <Fragment key={limpar}>
+      <MessageModal
+        close={() => {
+          setModalActive((value) => ({ ...value, show: false }));
+          setLimpar((value) => value + 1);
+        }}
+        {...modalActive}
+      />
+      <Form submit={(body) => mutate({ body })} isLoading={false} />
+    </Fragment>
+  );
 }
--- src/pages/poco/management.tsx
+++ src/pages/poco/management.tsx
@@ -1,25 +1,14 @@
-import { Fragment } from "react";
+import { Fragment, ReactNode, useState } from "react";
 import Pageheader from "../../components/pageheader/pageheader";
 import { Link } from "react-router-dom";
 import { queryClient, service } from "../../service";
 import { useTranslation } from "../../hooks";
 import { enqueueSnackbar } from "notistack";
-import { Card } from "react-bootstrap";
+import { Button, Card, Table } from "react-bootstrap";
 import { Column, CustomTable } from "../../components/common/table";
 
 const img =
   "https://hips.hearstapps.com/hmg-prod/images/robert-pattinson-as-bruce-wayne-batman-in-the-batman-1645187114.jpg";
-
-const LeftAction = (context: any) => (
-  <div className="hstack gap-2 fs-15">
-    <Link to="#" className="btn btn-icon btn-sm btn-light">
-      <i className="ri-download-2-line"></i>
-    </Link>
-    <Link to="#" className="btn btn-icon btn-sm btn-light">
-      <i className="ri-edit-line"></i>
-    </Link>
-  </div>
-);
 
 const RightAction = (context: any) => (
   <div className="hstack gap-2 fs-15">
@@ -44,34 +33,24 @@ export function PocoManagement() {
     },
   });
   const pocos = data?.body || [];
-  /**
- * "usuarioRegistroId": 1000,
-    "dataRegistro": "2024-02-15T23:51:42.4742758",
-    "atualizadoPor": 1000,
-    "dataAtualizacao": "2024-02-15T23:51:42.4742909",
-    "pocoId": 1,
-    "descricao": "AB2B",
-    "bacia": "test",
-    "bloco": "b2",
-    "licencaExploracao": "test",
-    "latitude": 1222,
-    "longetitude": 1111,
-    "lamineDeAgua": "2222",
-    "dataInicioPerfuracao": "2024-02-15T00:00:00",
-    "dataConclusaoPerfuracao": "2024-02-17T00:00:00",
-    "profundidade": 10,
-    "profundidadeTotal": 2333,
-    "nomeDaRig": "test",
-    "classe": "test",
-    "ambiente": "test",
-    "dataDt": "2024-03-02T00:00:00",
-    "dataDeReentrada": "2024-02-15T00:00:00",
-    "dataDeConclusao": "2024-02-21T00:00:00",
-    "pocoTipoId": 1,
-    "plataformaId": 2,
-    "provinciaId": 2,
-    "imagem": null
- */
+  const [pocoId, setPocoId] = useState(0);
+  const [visibility, setVisibility] = useState(1);
+
+  const LeftAction = (context: any) => (
+    <div className="hstack gap-2 fs-15">
+      <Link
+        to="#"
+        className="btn btn-icon btn-sm btn-light"
+        onClick={() => {
+          setPocoId(context.pocoId);
+          setVisibility(2);
+        }}
+      >
+        <i className="ri-focus-line"></i>
+      </Link>
+    </div>
+  );
+
   const columns: Column[] = [
     { title: "Descrição", dataKey: "descricao", display: true },
     { title: "Profundidade", dataKey: "profundidade", display: true },
@@ -83,18 +62,109 @@ export function PocoManagement() {
 
   return (
     <Fragment>
-      <Pageheader title="Gestão de poços" heading="Poços" active="gestão" />
-      <Card className={"m-5 p-3"}>
-        <CustomTable
-          data={pocos}
-          columns={columns}
-          leftAction={LeftAction}
-          rightAction={RightAction}
-        />
-      </Card>
+      {visibility === 1 ? (
+        <>
+          <Pageheader
+            title="Gestão de poços"
+            heading="Poços"
+            active="listagem"
+          />
+          <Card className={"m-5 p-3"}>
+            <CustomTable
+              data={pocos}
+              columns={columns}
+              leftAction={LeftAction}
+            />
+          </Card>
+        </>
+      ) : (
+        <>
+          <DetalhesPoco data={pocos.find((poco) => poco.pocoId === pocoId)} />
+          <Card className={"m-5 p-3"}>
+            <Button variant="dark" onClick={() => setVisibility(1)}>
+              Voltar
+            </Button>
+          </Card>
+        </>
+      )}
     </Fragment>
   );
 }
+
+function DetalhesPoco({ data }: { data: any }) {
+  const columns: Column[] = [
+    { title: "ID de Usuário", dataKey: "usuarioRegistroId", display: true },
+    { title: "Data de Registro", dataKey: "dataRegistro", display: true },
+    { title: "Atualizado Por", dataKey: "atualizadoPor", display: true },
+    { title: "Data de Atualização", dataKey: "dataAtualizacao", display: true },
+    { title: "ID do Poço", dataKey: "pocoId", display: true },
+    { title: "Descrição", dataKey: "descricao", display: true },
+    { title: "Bacia", dataKey: "bacia", display: true },
+    { title: "Bloco", dataKey: "bloco", display: true },
+    {
+      title: "Licença de Exploração",
+      dataKey: "licencaExploracao",
+      display: true,
+    },
+    { title: "Latitude", dataKey: "latitude", display: true },
+    { title: "Longitude", dataKey: "longetitude", display: true },
+    { title: "Lâmina de Água", dataKey: "lamineDeAgua", display: true },
+    {
+      title: "Data de Início da Perfuração",
+      dataKey: "dataInicioPerfuracao",
+      display: true,
+    },
+    {
+      title: "Data de Conclusão da Perfuração",
+      dataKey: "dataConclusaoPerfuracao",
+      display: true,
+    },
+    { title: "Profundidade", dataKey: "profundidade", display: true },
+    {
+      title: "Profundidade Total",
+      dataKey: "profundidadeTotal",
+      display: true,
+    },
+    { title: "Nome da Rig", dataKey: "nomeDaRig", display: true },
+    { title: "Classe", dataKey: "classe", display: true },
+    { title: "Ambiente", dataKey: "ambiente", display: true },
+    { title: "Data Dt", dataKey: "dataDt", display: true },
+    { title: "Data de Reentrada", dataKey: "dataDeReentrada", display: true },
+    { title: "Data de Conclusão", dataKey: "dataDeConclusao", display: true },
+    { title: "ID do Tipo de Poço", dataKey: "pocoTipoId", display: true },
+    { title: "ID da Plataforma", dataKey: "plataformaId", display: true },
+    { title: "ID da Província", dataKey: "provinciaId", display: true },
+  ];
+  return (
+    <>
+      <Pageheader title="Detalhes do poço" heading="poço" active="detalhes" />
+      <Card className={"m-5 p-3"}>
+        <TabelaColum data={data} columns={columns} />
+      </Card>
+    </>
+  );
+}
+
+interface TableColumProps {
+  data: Record<string, string | number>;
+  columns: { title: ReactNode; dataKey: string }[];
+}
+
+const TabelaColum: React.FC<TableColumProps> = ({ data, columns }) => {
+  return (
+    <Table>
+      <tbody>
+        {columns.map((column, index) => (
+          <tr key={index}>
+            <td>{column.title}</td>
+            <td>{data[column.dataKey]}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
 /**
  * import { Fragment, useState, useEffect, ReactNode } from "react";
 import { Card, Col, Row, Table, Button } from "react-bootstrap";
